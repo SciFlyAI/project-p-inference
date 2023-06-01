d12a2c83b0c2333f6ce52154cf1ff6b815722927
Vladimir Valeyev <ValV@users.noreply.github.com>
2023-06-01 22:10:00 +0300

    Unpin dependencies versions for Python 3.6

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     author_email='0x05a4@gmail.com',
     description='ProjectP inference package',
     install_requires=[
-        'ensemble-boxes==1.0.9',
-        'filetype==1.2.0',
-        'matplotlib==3.6.2',
-        'numpy==1.23.5',
-        'onnxruntime==1.14.1',
-        'opencv-python-headless==4.7.0.72',
-        'pandas==1.5.0',
-        'tqdm==4.62.3'
+        'ensemble-boxes',
+        'filetype',
+        'matplotlib',
+        'numpy',
+        'onnxruntime',
+        'opencv-python-headless',
+        'pandas',
+        'tqdm'
     ]
 )
